b999632c10f00d0a954df15ade4feba43949d48f
SONU KUMAR <sonuk.gupta81@gmail.com>
2026-02-22 22:35:07 +0530

    Update GEMINI_ENDPOINT to use v1beta API

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ const API_CONFIG = {
 };
 
 const GEMINI_MODEL = 'gemini-1.5-flash';
-const GEMINI_ENDPOINT = `https://generativelanguage.googleapis.com/v1/models/${GEMINI_MODEL}:generateContent`;
+const GEMINI_ENDPOINT =
+  `https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}:generateContent`;
 
 /***********************
  * STATE VARIABLES
